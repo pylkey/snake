c6d9c8fca0604832b4293aca84303dfd1f07b82c
Pylkey <pylkey@gmail.com>
2018-01-01 14:25:39 +0500

    improve my code

--- snake/Point.cs
+++ snake/Point.cs
@@ -11,5 +11,11 @@ namespace snake
         public int x;
         public int y;
         public char sym;
+        
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
     }
 }
--- snake/Snake.cs
+++ snake/Snake.cs
@@ -14,34 +14,16 @@ namespace snake
             p1.x = 1;
             p1.y = 3;
             p1.sym = '*';
-            Draw(p1.x, p1.y, p1.sym);
+            p1.Draw();
 
-
-            /* int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-
-            Draw(x1, y1, sym1);*/
 
             Point p2 = new Point();
             p2.x = 4;
             p2.y = 5;
             p2.sym = '#';
-            Draw(p2.x, p2.y, p2.sym);
-
-            /*int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);*/
+            p2.Draw();
 
             Console.Read();
         }    
-
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
     }
 }
