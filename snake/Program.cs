0e19316fc08c0e4d4a7febb40572326edee584b0
Pylkey <pylkey@gmail.com>
2018-01-11 23:49:44 +0500

    Snake is Moving

--- snake/Program.cs
+++ snake/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,7 +27,16 @@ namespace snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
-
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
             Console.Read();
         }    
     }
--- snake/Snake.cs
+++ snake/Snake.cs
@@ -7,8 +7,11 @@ namespace snake
 {
     class Snake : Figure
     {
-        public Snake(Point tall, int lenght, Direction direction)
+        Direction direction;
+
+        public Snake(Point tall, int lenght, Direction _direction)
         {
+            direction = _direction;
             pList = new List<Point>();
             for(int i = 0; i < lenght; i++)
             {
@@ -17,5 +20,25 @@ namespace snake
                 pList.Add(p);
             }
         }
+
+        internal void Move()
+        {
+            Point tail = pList.First();
+            pList.Remove(tail);
+            Point head = GetNextPoint();
+            pList.Add(head);
+
+            tail.Clear();
+            head.Draw();
+        }
+
+        public Point GetNextPoint()
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, direction);
+            return nextPoint;
+        }
+
     }
 }
